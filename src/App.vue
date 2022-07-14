f3220676f98eee87b6125331f3fcfbae0140341a
Toyin Jolaoso <oluwatoyinjolaoso@gmail.com>
2022-07-14 04:02:00 +0100

    Board and Navbar setup

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TheNavbar from './components/TheNavabr.vue'
+import TheDrawer from './components/TheDrawer.vue'
+</script>
 
 <template>
+  <TheNavbar />
   <RouterView />
+  <TheDrawer />
 </template>
---
+++ src/pages/boards/index.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import type { Board } from '../../types'
+import { ref } from 'vue'
+
+const boards = ref<Partial<Board>[]>([
+  {
+    id: '1',
+    title: 'My First board',
+    order: '[]',
+    image: { downloadUrl: 'https://picsum.photos/480/270?board=1' },
+  },
+  {
+    id: '2',
+    title: 'My Second board',
+    order: '[]',
+    image: { downloadUrl: 'https://picsum.photos/480/270?board=2' },
+  },
+  {
+    id: '3',
+    title: 'My Third board',
+    order: '[]',
+    // image: { downloadUrl: 'https://picsum.photos/480/270?board=1' },
+  },
+])
+</script>
+
+<template>
+  <h1 class="mb-5 text-3xl">Boards</h1>
+  <div class="flex items-center gap-4">
+    <BoardCard v-for="board in boards" :key="board.id" :board="board" />
+    <h1>New Board +</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
